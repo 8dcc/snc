9a81c75f3921f0cb22cc7d9f54e2baa719fa6213
8dcc <8dcc.git@gmail.com>
2023-07-16 14:06:40 +0200

    Add initial server socket code

--- src/main.c
+++ src/main.c
@@ -1,32 +1,50 @@
 
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
-/**
- * @brief The PI number
- */
-#define PI 3.141592
+#include <unistd.h>    /* sleep, write, close */
+#include <arpa/inet.h> /* htonl, htons */
+#include <sys/socket.h>
+
+typedef struct sockaddr sockaddr;
+typedef struct sockaddr_in sockaddr_in;
+
+#define MSG "Ping"
 
 /**
- * Simple function for adding 2 integers.
- *
- * @todo Rename function
- * @brief Sum a + b
- * @param a, b The numbers to sum
- * @return Sum of a + b
- */
-int test_func(int a, int b) {
-    return a + b;
-}
-
-/**
- * @todo Argument handling
  * @brief Entry point of the program
  * @param argc Number of arguments
  * @param argv Vector of string arguments
  * @return Exit code
  */
-int main(int argc, char** argv) {
-    printf("Hello, world!\n");
+int main(/* int argc, char** argv */) {
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(sockaddr_in));
+
+    /* Fill the struct we just declared.
+     * See: https://www.gta.ufrj.br/ensino/eel878/sockets/sockaddr_inman.html */
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); /* htonl -> uint32_t */
+    server_addr.sin_port        = htons(1337);       /* htons -> uint16_t */
+
+    /* Bind socket file descriptor to the struct we just filled (but casted) */
+    bind(listen_fd, (sockaddr*)&server_addr, sizeof(sockaddr));
+
+    listen(listen_fd, 10);
+
+    int conn_fd = 0;
+    for (;;) {
+        conn_fd = accept(listen_fd, (sockaddr*)NULL, NULL);
+
+        write(conn_fd, MSG, strlen(MSG));
+
+        close(conn_fd);
+        sleep(1);
+    }
+
     return 0;
 }
-
