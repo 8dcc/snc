516570739aeee7006a0bb5bfbcc41db58811c481
8dcc <8dcc.git@gmail.com>
2025-07-05 21:46:03 +0200

    Add GPL license comments to 'args.c' and 'main.h'

--- src/include/args.h
+++ src/include/args.h
@@ -1,6 +1,8 @@
 /*
  * Copyright 2025 8dcc
  *
+ * This file is part of snc (Simple NetCat).
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or any later version.
